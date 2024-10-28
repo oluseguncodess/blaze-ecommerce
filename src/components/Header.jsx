10d5236db73cef9d9862f65bd61080d2115e8ed0
olusegun <olusegunbamgbelu755@gmail.com>
2024-10-28 11:18:42 +0100

    updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
 import {cart, kratos, logo, menu} from '../assets/index'
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     const navContent = [{id: 1, text: "Home"}, {id: 2, text: "Pages"}, {id: 3, text: "Shop"}, {id: 4, text: "Element"}, {id: 5, text: "Blog"}]
 
     const displayNav = navContent.map(nav => <li key={nav.id} className='text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-8 decoration-[3px] cursor-pointer duration-300'>{nav.text}</li>)
     return(
-        <div className="w-full h-[68px] sm:h-20 bg-white border-b border-b-gray-800 font-titleFont">
+        <div className="w-full h-[68px] sm:h-20 bg-white border-b border-b-gray-800 font-titleFont sticky top-0 z-50">
             <div className="sm:max-w-screen-xl h-full mx-auto flex items-center justify-between p-4 sm:p-0">
-                <div>
-                    <img src={logo} alt="logo" className='sm:w-16 sm:h-16 w-12 h-12 rounded-full' />
-                </div>
+                <Link to='/'> 
+                    <div>
+                        <img src={logo} alt="logo" className='sm:w-16 sm:h-16 w-12 h-12 rounded-full' />
+                    </div>
+                </Link>
                 <div className='hidden sm:block'>
                     <ul className='flex items-center gap-8'>
                        {displayNav}
--- src/components/ViewProduct.jsx
+++ src/components/ViewProduct.jsx
@@ -1,7 +1,55 @@
+import { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
+import { MdOutlineStar } from "react-icons/md";
 
 function ViewProduct()  {
+    const [details, setDetails] = useState({});
+    const location = useLocation();
+    useEffect(() => {
+        setDetails(location.state.item)
+    }, []);
+
   return (
-    <div>ViewProduct</div>
+    <div>
+        <div className="max-w-screen-xl mx-auto my-10 flex sm:gap-10 gap-5">
+            <div className="w-2/5 relative">
+                <img src={details.image} alt="productImg" className="w-full h-[250px] sm:h-[550px] object-cover" />
+                <div className="absolute top-0 right-0">
+                    <p className="bg-black text-white font-semibold font-titleFont px-8 py-1">Sale</p>
+                </div>
+            </div>
+            <div className="w-3/5">
+                <div className="flex gap-4 flex-col mt-3">
+                    <h2>{details.title}</h2>
+                    <div>
+                        <p className="text-2xl font-medium text-gray-900">${details.price}</p>
+                    </div>
+                    <div className="flex items-center gap-2 text-base">
+                        <div className="flex">
+                            <MdOutlineStar/>
+                            <MdOutlineStar/>
+                            <MdOutlineStar/>
+                            <MdOutlineStar/>
+                            <MdOutlineStar/>
+                        </div>
+                        <p className="text-gray-500 text-xs sm:text-sm">1 customer review</p>
+                    </div>
+                    <p className="text-sm sm:text-base text-gray-500 mt-3">{details.description}</p>
+                    <div className="pt-8 sm:flex sm:gap-y-4">
+                        <div className="w-52 flex items-center justify-between text-gray-500 gap-4 border p-3 border-gray-800 mb-8 sm:mb-0">
+                            <p className="text-xs sm:text-sm">Quantity</p>
+                            <div className="flex items-center gap-4 text-sm font-semibold">
+                                <button className="border h-5 font-normal text-lg flex items-center justify-center px-2 hover:bg-gray-700 hover:text-white cursor-pointer duration-300 active:bg-black">-</button>
+                                <span>{1}</span>
+                                <button className="border h-5 font-normal text-lg flex items-center justify-center px-2 hover:bg-gray-700 hover:text-white cursor-pointer duration-300 active:bg-black">+</button>
+                            </div>
+                        </div>
+                        <button className="bg-black text-white py-3 px-6 active:bg-gray-800">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
